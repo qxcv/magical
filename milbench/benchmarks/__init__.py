f276ba5e0cddf4bc831bba21078b1db9a45e2140
Sam Toyer <sam@qxcv.net>
2020-03-02 10:06:12 -0800

    Comment

--- milbench/benchmarks/__init__.py
+++ milbench/benchmarks/__init__.py
@@ -213,7 +213,7 @@ def register_envs():
 
     # Long episodes because this is a hard environment. You can have up to 12
     # blocks when doing random layouts, and that takes a human 30-35s to
-    # process (so 650/15=43.3s is just enough time to finish a 12-block run if
+    # process (so 320/8=40s is just enough time to finish a 12-block run if
     # you know what you're doing).
     cluster_ep_len = 320
     cluster_variants = []
